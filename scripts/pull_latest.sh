1a0d7b06215e0628de9d9eb6409ac1cbcffb2fa8
Raj Singh <raj.singh@rajdoesml.com>
2026-01-10 05:32:11 -0500

    cleaning

--- scripts/pull_latest.sh
+++ scripts/pull_latest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Pull latest code on RunPod instance
-# Replaces /app/src with latest from GitHub
+# Replaces /app with latest from GitHub
 
-cd /app && rm -rf src && git clone --depth 1 https://github.com/rajs96/music_audio_analyzer.git /tmp/repo && cp -r /tmp/repo/src . && rm -rf /tmp/repo
+git clone --depth 1 https://github.com/rajs96/music_audio_analyzer.git /tmp/repo && cp -r /tmp/repo/* /app/ && rm -rf /tmp/repo
 
-echo "Done! Latest src/ pulled from GitHub."
+echo "Done! Latest repo pulled from GitHub."
